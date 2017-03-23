02233fa97c5f0a2d756fc5b3a2228091b24a44e6
Jiri Tobisek <jiri@wix.com>
2017-03-23 14:02:10 +0200

    select() can deal with two ids on one element. Resolves #5

--- src/select-dom.ts
+++ src/select-dom.ts
@@ -1,27 +1,27 @@
-export function selectDom<T extends Element>(container: T, attrName: string = 'data-automation-id'): (...selectors: string[]) => T | null {
+export function selectDom(container: Element, attrName: string = 'data-automation-id') {
 
-    function select(parentElement: T, ...selectors: string[]): T | null {
+    function select<T extends Element>(parentElement: Element, ...selectors: string[]): T | null {
         const [selector, ...rest] = selectors;
         const selectorExpr = `[${attrName}~="${selector}"]`;
         const elementList: T[] = Array.prototype.slice.call(parentElement.querySelectorAll(selectorExpr));
         if(parentElement.matches(selectorExpr)) {
-            elementList.unshift(parentElement);
+            elementList.unshift(parentElement as T);
         }
         if(elementList.length === 0) {
             return null;
-        } else if(elementList.length === 1) {
+        } else if (elementList.length === 1) {
             const element = elementList[0];
-            if(rest.length>0) {
-                return select(element, ...rest);
+            if (rest.length > 0) {
+                return select<T>(element, ...rest);
             } else {
-                return element;
+                return element as T;
             }
         } else {
             throw new Error(`Selector "${selector}" ambiguous (${elementList.length} matches)`);
         }
     }
 
-    return function (...selectors: string[]): T | null {
-        return select(container, ...selectors);
+    return function <T extends Element>(...selectors: string[]) {
+        return select<T>(container, ...selectors);
     }
 }
