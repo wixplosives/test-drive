7c5c7f517b1053512d0df5da6a8a648f0905e746
Avi Vahl <avi.vahl@wix.com>
2017-08-03 18:20:35 +0300

    Add chai-style for easier style matching https://github.com/darlanmendonca/chai-style

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import chaiDom = require('chai-dom');
 import isPresent from './is-present';
 import chaiAsPromised = require('chai-as-promised-compat');
 import sinonChai = require('sinon-chai');
+import chaiStyle = require('chai-style');
 import layout from './layout';
 chai.use(layout);
 chai.use(sinonChai);
@@ -13,6 +14,7 @@ chai.use(chaiAsPromised);
 chai.use(isPresent);
 if (typeof window !== 'undefined') { // chai-dom's matchers are not Node-safe
     chai.use(chaiDom);
+    chai.use(chaiStyle);
 }
 
 const expect = Object.assign(chai.expect.bind(chai), chai.expect) as Chai.ExpectStatic;
---
+++ typings/chai-style.d.ts
@@ -0,0 +1,4 @@
+declare module 'chai-style' {
+    function chaiStyle(chai: any, utils: any): void;
+    export = chaiStyle;
+}
