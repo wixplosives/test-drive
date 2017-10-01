b406e4e8fb06093e5159df713f8a233cbbb7a17b
Avi Vahl <avi.vahl@wix.com>
2017-10-01 18:01:38 +0300

    Add missing karma-firefox-launcher and commit forgotten files for chai-as-promised change

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,18 @@ import sinon = require('sinon');
 import chai = require('chai');
 import chaiDom = require('chai-dom');
 import isPresent from './is-present';
-import chaiAsPromised = require('chai-as-promised-compat');
+import chaiAsPromised = require('chai-as-promised');
+import chaiStyle = require('chai-style');
 import sinonChai = require('sinon-chai');
 import layout from './layout';
+
 chai.use(layout);
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 chai.use(isPresent);
 if (typeof window !== 'undefined') { // chai-dom's matchers are not Node-safe
     chai.use(chaiDom);
+    chai.use(chaiStyle);
 }
 
 const expect = Object.assign(chai.expect.bind(chai), chai.expect) as Chai.ExpectStatic;
