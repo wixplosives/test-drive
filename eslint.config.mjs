b6f19ff013a8fb7958d04eeb306dcbf43736eeee
Avi Vahl <avi.vahl@wix.com>
2024-08-04 18:07:13 +0300

    infra: migrate to eslint@9

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import { fixupPluginRules } from '@eslint/compat';
+import pluginJs from '@eslint/js';
+import configPrettier from 'eslint-config-prettier';
+import pluginNoOnlyTests from 'eslint-plugin-no-only-tests';
+import pluginTypescript from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ['**/dist/', '**/*.{js,mjs,cjs}'] },
+  pluginJs.configs.recommended,
+
+  ...pluginTypescript.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+
+  {
+    plugins: {
+      'no-only-tests': fixupPluginRules(pluginNoOnlyTests),
+    },
+  },
+  configPrettier,
+
+  {
+    rules: {
+      'no-console': 'error',
+      'no-only-tests/no-only-tests': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }], // allow unused _ variables
+    },
+  },
+];
