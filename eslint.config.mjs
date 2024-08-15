43661ffd59aa325d017800244e7e2b9b3ea26b34
Avi Vahl <avi.vahl@wix.com>
2024-08-15 14:59:10 +0300

    infra: re-enable lint for js/mjs/cjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,26 +4,30 @@ import configPrettier from 'eslint-config-prettier';
 import pluginNoOnlyTests from 'eslint-plugin-no-only-tests';
 import pluginTypescript from 'typescript-eslint';
 
+for (const config of pluginTypescript.configs.recommendedTypeChecked) {
+  config.files = ['**/*.{ts,tsx,mts,cts}']; // ensure config only targets TypeScript files
+}
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['dist/', 'cjs/', 'esm/', '**/*.{js,mjs,cjs}'] },
+  { ignores: ['dist/', 'cjs/', 'esm/'] },
   pluginJs.configs.recommended,
-
-  ...pluginTypescript.configs.recommendedTypeChecked,
-  { languageOptions: { parserOptions: { projectService: true } } },
-
-  {
-    plugins: {
-      'no-only-tests': fixupPluginRules(pluginNoOnlyTests),
-    },
-  },
-  configPrettier,
-
+  { plugins: { 'no-only-tests': fixupPluginRules(pluginNoOnlyTests) } },
   {
     rules: {
       'no-console': 'error',
       'no-only-tests/no-only-tests': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }], // allow unused _ variables
+      'no-undef': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     },
   },
+  ...pluginTypescript.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+  {
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+  configPrettier,
 ];
